3ffcfc5b95c5ed612b42a3bbba2fd59abd37e274
Ilya Bagleybter <bagleyilya@gmail.com>
2015-03-27 13:36:22 +0000

    fixed a bug which made apple spawning break

--- game.py
+++ game.py
@@ -48,8 +48,8 @@ def gameLoop():                                       #starts the game loop wher
 
     
     
-    randAppleX = round(random.randrange(0, display_width-block_size)#/10.0)*10.0           #random apple spawn locations
-    randAppleY = round(random.randrange(0, display_height-block_size#)/10.0)*10.0
+    randAppleX = round(random.randrange(0, display_width-block_size))#/10.0)*10.0           #random apple spawn locations
+    randAppleY = round(random.randrange(0, display_height-block_size))#/10.0)*10.0
     
     while not gameExit:
 
@@ -152,8 +152,8 @@ def gameLoop():                                       #starts the game loop wher
 
         if lead_x >= randAppleX and lead_x <= randAppleX + AppleThickness:
             if lead_y >= randAppleY and lead_y <= randAppleY + AppleThickness:
-                randAppleX = round(random.randrange(0, display_width-block_size)#/10.0)*10.0
-                randAppleY = round(random.randrange(0, display_height-block_size)#/10.0)*10.0
+                randAppleX = round(random.randrange(0, display_width-block_size))#/10.0)*10.0
+                randAppleY = round(random.randrange(0, display_height-block_size))#/10.0)*10.0
                 snakeLength += 1
 
 
