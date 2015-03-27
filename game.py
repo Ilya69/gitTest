26ee15a322bb74f835ac3196a586654450174cee
Ilya Bagleybter <bagleyilya@gmail.com>
2015-03-27 14:14:17 +0000

    text at the game-over screen is now centered

--- game.py
+++ game.py
@@ -29,9 +29,28 @@ def snake(block_size, snakelist):                     #defines the snake functio
      for XnY in snakelist:
          pygame.draw.rect(gameDisplay, green, [XnY[0],XnY[1],block_size,block_size])
 
-def message_to_screen(msg, color):                     #defines the message function that displays stuff to the user
-    screen_text = font.render(msg, True, color)         
-    gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+
+
+
+def text_objects(text, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
+
+
+
+
+def message_to_screen(msg, color):
+    textSurf, textRect = text_objects(msg, color)
+                                                                   #defines the message function that displays stuff to the user
+    # screen_text = font.render(msg, True, color)
+    # gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+
+    textRect.center = (display_width / 2), (display_height /2)
+    gameDisplay.blit(textSurf, textRect)
+
+
+
 
 
 def gameLoop():                                       #starts the game loop where the main functions are called and where most of the cool shit happens
